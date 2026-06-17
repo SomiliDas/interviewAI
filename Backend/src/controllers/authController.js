9adc53d8727cbd9e277ee3955bd04e0776b949d2
SomiliDas <somili.das12@gmail.com>
2026-06-17 23:49:34 +0530

    Enhance cookie settings for JWT token

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -43,7 +43,12 @@ const registerUserController = async (req, res)=>{
             username : user.username
         }, process.env.JWT_SECRET, {expiresIn : "7d"})
 
-        res.cookie("token", token)
+        res.cookie("token", token , {
+                    httpOnly: true,
+                    secure: true,
+                    sameSite: "none",
+                    maxAge: 7 * 24 * 60 * 60 * 1000
+                })
 
 
         return res.status(201).json({
@@ -93,7 +98,12 @@ const loginUserController = async (req, res)=>{
         {expiresIn : "7d"}
         )
 
-        res.cookie("token", token)
+        res.cookie("token", token , {
+                        httpOnly: true,
+                        secure: true,
+                        sameSite: "none",
+                        maxAge: 7 * 24 * 60 * 60 * 1000
+                    })
             
         return res.status(200).json({
             message : "user logged in successfully",
